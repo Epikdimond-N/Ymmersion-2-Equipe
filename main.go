e7ad3b455e8487fdae70ccdbb603082b7c0b3995
Nicolas <nicolas.moyon@ynov.com>
2024-01-08 16:58:09 +0100

    update json writing and deleting items

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	routeur "onepiece/routeur"
-	initTemplate "onepiece/temp"
+	One "onepiece/controller"
 )
 
 func main() {
-	initTemplate.InitTemplate()
-	routeur.InitServe()
+	//initTemplate.InitTemplate()
+	//routeur.InitServe()
 	//One.GetArcs()
 	//One.GetChar()
 	//One.GetEvents()
+	One.UpdateChar()
 }
